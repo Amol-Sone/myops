93585c4ae753b498eb94c5890def704a14f62a6e
jpagnano <ajulian@jpagnano.com>
2017-02-15 10:33:47 -0500

    add ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,46 +1,46 @@
 module.exports = {
-		/**
-		 * Application configuration section
-		 * http://pm2.keymetrics.io/docs/usage/application-declaration/
-		 */
-		apps: [
+	/**
+	 * Application configuration section
+	 * http://pm2.keymetrics.io/docs/usage/application-declaration/
+	 */
+	apps: [
 
-			// First application
-			{
-				name: "myops",
-				script: "keystone.js",
-				env: {
-					COMMON_VARIABLE: "true"
-				},
-				env_production: {
-					NODE_ENV: "production"
-				}
-			}
-		],
-
-		/**
-		 * Deployment section
-		 * http://pm2.keymetrics.io/docs/usage/deployment/
-		 */
-		deploy: {
-			production: {
-				user: "opper",
-				host: "162.243.172.195",
-				ref: "origin/master",
-				repo: "https://github.com/jpagnano/myops.git",
-				path: "/home/www/myops.me",
-				"post-deploy": "npm install && pm2 startOrRestart ecosystem.json --env production"
+		// First application
+		{
+			name: "myops",
+			script: "keystone.js",
+			env: {
+				COMMON_VARIABLE: "true"
 			},
-			dev: {
-      user: "opper",
-      host: "162.243.172.195",
-      ref: "origin/master",
-      repo: "https://github.com/jpagnano/myops.git",
-      path: "/home/www/myops.me",
-      "post-deploy": "npm install && pm2 startOrRestart ecosystem.json --env dev"
-				env: {
-					NODE_ENV: "dev"
-				}
+			env_production: {
+				NODE_ENV: "production"
 			}
 		}
-  }
+	],
+
+	/**
+	 * Deployment section
+	 * http://pm2.keymetrics.io/docs/usage/deployment/
+	 */
+	deploy: {
+		production: {
+			user: "opper",
+			host: "162.243.172.195",
+			ref: "origin/master",
+			repo: "https://github.com/jpagnano/myops.git",
+			path: "/home/opper/www/myops.me",
+			"post-deploy": "npm install && pm2 startOrRestart ecosystem.json --env production"
+		},
+		dev: {
+			user: "opper",
+			host: "162.243.172.195",
+			ref: "origin/master",
+			repo: "https://github.com/jpagnano/myops.git",
+			path: "/home/opper/www/myops.me",
+			"post-deploy": "npm install && pm2 startOrRestart ecosystem.json --env dev"
+			env: {
+				NODE_ENV: "dev"
+			},
+		}
+	}
+}
